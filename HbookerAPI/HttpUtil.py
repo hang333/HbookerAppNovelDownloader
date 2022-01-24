0783fa2f5f3fd717a816b3f3aa6a2c02818b6184
w-e-w <40751091+w-e-w@users.noreply.github.com>
2022-01-25 06:28:03 +0900

    attempt fix / debug hang bug

--- HbookerAPI/HttpUtil.py
+++ HbookerAPI/HttpUtil.py
@@ -10,23 +10,27 @@ def get(url, params=None, retry=maxRetry, **kwargs):
     for count in range(retry):
         try:
             return str(requests.get(url, params=params, headers=headers, **kwargs).text)
-        except (OSError, TimeoutError, IOError) as e:
+        except (OSError, TimeoutError, IOError, requests.exceptions.Timeout) as e:
             if retry != retry - 1:
                 print("\nGet Error Retry: " + str(e) + '\n' + url)
                 time.sleep(1 * count)
             else:
                 print("\nGet Failed: " + str(e) + '\n' + url + "\nTerminating......")
                 sys.exit(1)
+        except Exception as e:
+            print(e)
 
 
 def post(url, data=None, retry=maxRetry, **kwargs):
     for count in range(retry):
         try:
             return str(requests.post(url, data, headers=headers, **kwargs).text)
-        except (OSError, TimeoutError, IOError) as e:
+        except (OSError, TimeoutError, IOError, requests.exceptions.Timeout) as e:
             if retry != retry - 1:
                 print("\nGet Error Retry: " + str(e) + '\n' + url)
                 time.sleep(1 * count)
             else:
                 print("\nPost Failed: " + str(e) + '\n' + url + "\nTerminating......")
                 sys.exit(1)
+        except Exception as e:
+            print(e)
