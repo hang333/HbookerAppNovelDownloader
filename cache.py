2b84d0c10e49b38a88d93017a402c40010a94d7b
VeronicaAlexia <Elaina-Alex@proton.me>
2022-10-25 15:57:01 +0800

    ~

--- cache.py
+++ cache.py
@@ -1,5 +1,5 @@
-import json
-import os
+from instance import *
+import book
 
 
 def save_cache(book_id: str, response: dict) -> None:
@@ -15,3 +15,13 @@ def load_cache(book_id: str) -> dict:
             return json.load(book_info_file)
     else:
         print(f'{book_id}.json not found.')
+
+
+def test_cache_and_init_object(book_id: str) -> bool:
+    Vars.current_book = load_cache(book_id)
+    if not isinstance(Vars.current_book, dict):
+        print("服务器无法获取书籍信息，已尝试从本地缓存中获取，但未找到。")
+        return False
+    Vars.current_book = book.Book(None, Vars.current_book)
+    print("服务器无法获取书籍信息，从本地缓存中获取书籍信息成功。")
+    return True
